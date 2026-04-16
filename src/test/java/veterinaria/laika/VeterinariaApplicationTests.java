a0e7241cca35716faadf461b4f2dbe27c565484c
leonardoclaros01 <Leonardo.claros01@unicatolica.edu.co>
2026-04-16 10:44:00 -0500

    Initial commit

---
+++ src/test/java/veterinaria/laika/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package veterinaria.laika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
